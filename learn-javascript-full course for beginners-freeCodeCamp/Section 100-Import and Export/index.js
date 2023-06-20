69708a9ba3ed3842c88cda343d5550c48259855e
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-06-20 08:42:54 -0500

    Add files via upload

--- learn-javascript-full course for beginners-freeCodeCamp/Section 100-Import and Export/index.js
+++ learn-javascript-full course for beginners-freeCodeCamp/Section 100-Import and Export/index.js
@@ -7,10 +7,24 @@ const cap = capitalizeString("hello!");
 console.log(cap);
 
 /*****USE EXPORT TO REUSE A CODE BLOCK******/
-// You export functions and variables from one file so that you can import them into another file
+// You export functions and variables from one file so that you can import them into another file so you can reuse different code
 const capitalizeString = (string) => {
     return string.charAt(0).toUpperCase() + string.slicer(1);
-
-    const foo = "bar";
-    const bar = "foo";
 }
+
+export { capitalizeString };
+
+export const foo = "bar";
+export const bar = "foo";
+
+/*****USE * TO IMPORT EVERYTHING FROM A FILE******/
+// when importing everything you must create an object to store everything in. 
+import * as capitalizeStrings from "capitalize_strings";
+
+/*****CREATE AN EXPORT FALLBACK WITH EXPORT DEFAULT******/
+export default function subtract(x,y) { return x - y; }
+
+/*****IMPORT A DEFAULT EXPORT******/
+import subtract from "math_functions";
+
+subtract(7,4);
